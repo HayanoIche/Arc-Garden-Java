7065ddb785b133cd9678554f3c19473f6df10626
HayanoIche <ayanoiichi@gmail.com>
2026-05-14 18:06:54 -0300

    Mais métodos adicionados a classe jardim

--- src/br/com/tdspk/arcgarden/model/Jardim.java
+++ src/br/com/tdspk/arcgarden/model/Jardim.java
@@ -2,6 +2,10 @@ package br.com.tdspk.arcgarden.model;
 
 import java.time.LocalDate;
 
+//
+// Classe que maneja o jardim do jogo
+//
+
 public class Jardim
 {
     // Atributos
@@ -24,7 +28,32 @@ public class Jardim
     public void setTema(int tema) { this.tema = tema; }
 
     // Metódos Workers
+    // Metodo para adicionar uma planta nova ao jardim
     public void ganharPlanta(Planta planta)
+    {
+
+    }
+
+    // Metodo para mostrar todas as plantas que estão no jardim
+    public void mostrarPlantas()
+    {
+
+    }
+
+    // Metodo que atualiza as plantas sempre que o jogador fica muito tempo sem regar
+    public void atualizarPlantas()
+    {
+
+    }
+
+    // Metodo usado pra calcular o score total do jardim pra saber o ranking global dele
+    public void calcularScoreTotal()
+    {
+
+    }
+
+    // Metodo que roda a parte principal que é a "gameplay" do jardim
+    public void Jogar()
     {
 
     }
