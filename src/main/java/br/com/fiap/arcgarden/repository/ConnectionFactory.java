efb4688bafdb91b91b1a2a55e7b3b3ec45adf203
Igor Hayano <167532390+HayanoIche@users.noreply.github.com>
2026-09-09 16:39:18 -0300

    feat: connection factory criado

---
+++ src/main/java/br/com/fiap/arcgarden/repository/ConnectionFactory.java
@@ -0,0 +1,42 @@
+package br.com.fiap.arcgarden.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Teste de conexão...");
+
+        try (Connection con = ConnectionFactory.getConnection()) {
+            if (con != null && !con.isClosed()) {
+                System.out.println("Conexão estabelecida com sucesso!");
+                System.out.println("Banco: " + con.getMetaData().getDatabaseProductName());
+                System.out.println("Versão: " + con.getMetaData().getDatabaseProductVersion());
+            }
+        } catch (SQLException e) {
+            System.err.println("Falha ao fechar ou validar a conexão.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Erro ao conectar ao banco de dados:");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    // Atributos Hardcoded
+    private static String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
+    private static String USER = "RM574069";
+    private static String PASS = "210208";
+
+    public static Connection getConnection()
+    {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
