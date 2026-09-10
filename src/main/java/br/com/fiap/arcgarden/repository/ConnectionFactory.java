2f5f7b02f8190c9c3fdcdc0a32cf918745461855
Igor Hayano <ayanoiichi@gmail.com>
2026-09-10 08:55:26 -0300

    update: conexão da connection factory

--- src/main/java/br/com/fiap/arcgarden/repository/ConnectionFactory.java
+++ src/main/java/br/com/fiap/arcgarden/repository/ConnectionFactory.java
@@ -27,8 +27,8 @@ public class ConnectionFactory
 
     // Atributos Hardcoded
     private static String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static String USER = "RM574069";
-    private static String PASS = "210208";
+    private static String USER = "RM571751";
+    private static String PASS = "280508";
 
     public static Connection getConnection()
     {
